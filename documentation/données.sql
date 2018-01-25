daba5288329a79676bf7d4e601ed1f21977402dc
vij59 <32669550+vij59@users.noreply.github.com>
2018-01-25 12:52:23 +0100

    Update données.sql

--- documentation/données.sql
+++ documentation/données.sql
@@ -1,24 +1,4 @@
---
--- PostgreSQL database dump
---
 
--- Dumped from database version 9.6.5
--- Dumped by pg_dump version 9.6.5
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET search_path = public, pg_catalog;
-
---
--- Data for Name: commentaire; Type: TABLE DATA; Schema: public; Owner: postgres
---
 
 INSERT INTO commentaire (id, id_utilisateur, id_topo, commentaire, date) VALUES (11, 1, 4, 'trou
 
